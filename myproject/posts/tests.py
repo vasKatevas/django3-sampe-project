13e02f1013c06b66b5ccaec600322b80038ac813
Anargyros Tsadimas <argiris@norbloc.com>
2021-04-04 22:59:08 +0300

    added sample test for posts model

--- myproject/posts/tests.py
+++ myproject/posts/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Posts
 
-# Create your tests here.
+class PostModelTests(TestCase):
+
+    def setUp(self):
+       User.objects.create(
+           first_name='MyName',
+           last_name="MNySurname",
+           email="testuser@test.com"
+       )
+
+    def test_post_add(self):
+        user = User.objects.get(pk=1)
+        post = Posts(userId=user, title='Test Post', body='Sample body')
+        post.save()
+        self.assertEquals(Posts.objects.get(pk=1).title, 'Test Post')
