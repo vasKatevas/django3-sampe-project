cf37535bb0fb9107af86cad2e87b60503369376d
Anargyros Tsadimas <tsadimas@hua.gr>
2021-04-05 12:44:52 +0300

    added post details

--- myproject/posts/views.py
+++ myproject/posts/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Posts
 
 def index(request):
     data = Posts.objects.all()
     context = {'data' : data}
     return render(request, 'posts/list.html', context)
+
+def detail(request, post_id):
+    post = get_object_or_404(Posts, pk=post_id)
+    return render(request, 'posts/detail.html', {'post': post})
